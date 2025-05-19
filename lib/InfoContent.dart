a33f9b5db0e082e1e519a8b9dde9231e21da03b2
augi07 <102184778+augi07@users.noreply.github.com>
2025-05-19 17:42:45 +0200

    make Info page and mahe reset Progress of counter in settings page

--- lib/InfoContent.dart
+++ lib/InfoContent.dart
@@ -3,8 +3,18 @@ import 'package:flutter/material.dart';
 class InfoContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Info Page'),
+    return Container(
+      width: 200, height: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        color: Colors.white,
+      ),
+      child: Text(
+        textAlign: TextAlign.center,
+        'Info page',
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: Colors.black ),
+        
+      ),
     );
   }
 }
